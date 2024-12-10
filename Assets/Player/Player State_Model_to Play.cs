b17c2e3a74bd1ba2d9943f3f74c61839dfa6368b
DESKTOP-LQCSVON\Sotetsu <sakakiki.sousaku@gmail.com>
2024-12-11 00:26:09 +0900

    プレイヤー移動・目の位置調整（画面遷移中）完成

--- Assets/Player/Player State_Model_to Play.cs
+++ Assets/Player/Player State_Model_to Play.cs
@@ -6,10 +6,18 @@ public class PlayerState_Model_toPlay : PlayerStateBase_Model
     private Vector2 startPos;
     private Vector2 targetPos;
     private bool isMoving;
+    private float posCorrectionY;
+    private float velocityY;
+    private float gravity = 40;
+    private Vector2 eyeStartPos;
+    private Vector2 eyeStandbyPos;
+    private Vector2 eyePlayPos;
 
     public PlayerState_Model_toPlay(PlayerStateMachine stateMachine) : base(stateMachine)
     {
         targetPos = playerCon.playerPos_GameStart;
+        eyeStandbyPos = playerCon.eyePos_Model;
+        eyePlayPos = playerCon.eyePos_Play;
     }
 
     public override void Enter()
@@ -24,6 +32,9 @@ public class PlayerState_Model_toPlay : PlayerStateBase_Model
         if (tf.eulerAngles.z > 180)
             startEulerAnglesZ -= 360;
 
+        //目の位置を記憶
+        eyeStartPos = eyeTf.localPosition;
+
         //まっすぐ立っていれば小さく、そうでなければ大きくジャンプ
         rb.velocity = Vector2.up * (Mathf.Abs(startEulerAnglesZ) < 1 ? 7 : 12);
     }
@@ -36,26 +47,39 @@ public class PlayerState_Model_toPlay : PlayerStateBase_Model
         //回転
         rb.rotation = Mathf.Lerp(startEulerAnglesZ, 0, Mathf.Sqrt(elapsedTime * 2));
 
+        //目の位置調整
+        if (elapsedTime < 0.5)
+            eyeTf.localPosition = Vector2.Lerp(eyeStartPos, eyeStandbyPos, elapsedTime * 3);
+        else eyeTf.localPosition = Vector2.Lerp(eyeStandbyPos, eyePlayPos, (elapsedTime - 1) * 2);
+
         if (elapsedTime < 0.5) return;
 
         //スケール調整
         if (elapsedTime < 1)
             tf.localScale = (Vector3.one - Vector3.up * (elapsedTime - 0.5f)) * 1.5f;
-        else tf.localScale = Vector3.one * Mathf.Lerp(1.5f, 1, (elapsedTime - 1) * 2);
+        else tf.localScale = Vector3.one * Mathf.Lerp(1.5f, 1, elapsedTime - 1);
 
         if (elapsedTime < 1) return;
 
         //ジャンプ開始位置記憶
         if (!isMoving)
         {
+            //処理実行済みのフラグを立てる
             isMoving = true;
+
+            //物理演算を無効化
+            rb.isKinematic = true;
+
+            //移動関連の値を記憶
             startPos = tf.position;
+            posCorrectionY = 0;
+            velocityY = 20;
         }
 
-        //移動
-        tf.position = 
-            Vector2.Lerp(startPos, targetPos, elapsedTime - 1) + 
-            Vector2.up * Mathf.Sqrt(Mathf.Sin(Mathf.PI * (elapsedTime - 1))) * 5;
+        //移動関連演算
+        velocityY -= gravity * Time.deltaTime;
+        posCorrectionY += velocityY * Time.deltaTime;
+        tf.position = Vector2.Lerp(startPos, targetPos, elapsedTime - 1) + Vector2.up * posCorrectionY;
 
         //ゲームステートがPlayならステート遷移
         if (gameStateMachine.currentState == gameStateMachine.state_Play)
@@ -67,6 +91,9 @@ public class PlayerState_Model_toPlay : PlayerStateBase_Model
         //プレイヤーの当たり判定・描画レイヤー変更
         playerCon.SetLayer(0);
         playerCon.SetSortingLayer("Player");
+
+        //物理演算の有効化
+        rb.isKinematic = false;
 
         //重力スケール補正
         rb.gravityScale = 10;
